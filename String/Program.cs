aafb6e4738ee6a33adc446b1aa52d09949739796
KPriyadh <priyadharshini.sf4630@gmail.com>
2024-04-25 09:10:11 +0530

    Adding Basic C sharp Files

---
+++ String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace String{
+    class Program{
+        public static void Main(string[] args)
+        {
+            string a =Console.ReadLine();
+            string b = Console.ReadLine();
+            int count=0;
+            
+            
+            for (int i = 0; i < a.Length - b.Length + 1; i++)
+            {
+                
+                if (a.Substring(i, b.Length) == b)
+                {
+                    count++;
+                    
+                    
+                    
+                }
+                
+            
+            
+                
+            } 
+            Console.WriteLine(count);
+            
+
+            
+        }
+        }
+    }
